51b42b58293ce9f8ca41ad9224c499d2ce15ecbe
Daniel Distant <daniel.distant1@gmail.com>
2015-06-27 13:05:12 -0400

    Added formatters to printBoard

--- TicTacToe/TicTacToe/main.m
+++ TicTacToe/TicTacToe/main.m
@@ -18,8 +18,23 @@
 
 - (void) printBoard {
     
+    NSString *spaceTopLeft = @" ";
+    NSString *spaceTopMid = @" ";
+    NSString *spaceTopRight = @" ";
+    
+    NSString *spaceMidLeft = @" ";
+    NSString *spaceMidMid = @" ";
+    NSString *spaceMidRight = @" ";
+    
+    NSString *spaceBottomLeft = @" ";
+    NSString *spaceBottomMid = @" ";
+    NSString *spaceBottomRight = @" ";
+    
     //       top            mid         bottom
-    printf(" | | \n-----\n | | \n-----\n | | \n");
+    printf("%s|%s|%s\n-----\n%s|%s|%s\n-----\n%s|%s|%s\n", [spaceTopLeft UTF8String], [spaceTopMid UTF8String], [spaceTopRight UTF8String], [spaceMidLeft UTF8String], [spaceMidMid UTF8String], [spaceMidRight UTF8String], [spaceBottomLeft UTF8String], [spaceBottomMid UTF8String], [spaceBottomRight UTF8String]);
+
+    
+    
     
 }
 
@@ -32,6 +47,8 @@ int main(int argc, const char * argv[]) {
         tictactoe *game1 = [[tictactoe alloc] init];
         [game1 printBoard];
         
+//        NSString *emptySpace = @" ";
+//        printf("this is where the empty spa%sce goes:", [emptySpace UTF8String]);
     }
     return 0;
 }
