3a51882b0af77590d92c22594ac002ad9badae0b
Chrisrdavid7 <chrisrdavid7@gmail.com>
2015-06-27 17:30:52 -0400

    Add Second Pleyer

--- TicTacToe/TicTacToe/main.m
+++ TicTacToe/TicTacToe/main.m
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-@interface Player : NSObject
+@interface Player : NSObject // Player One Starts with X
 
 @end
 
@@ -17,7 +17,7 @@
 
 @end
 
-@interface Computer : NSObject
+@interface Computer : NSObject // Computer automatic plays in one player mode
 
 @end
 
@@ -32,7 +32,7 @@
 
 - (void) printBoard;
 
-- (void) setMove:(int)move;
+- (void) chooseMove:(int)move;
 
 
 @property (nonatomic) NSMutableArray *spaces;
@@ -66,17 +66,32 @@
     
     printf("\n\n");
     
+    //Tic Tac Toe Board
+    
     printf("%s|%s|%s\n-----\n%s|%s|%s\n-----\n%s|%s|%s\n", [_spaces[0] UTF8String], [_spaces[1] UTF8String], [_spaces[2] UTF8String], [_spaces[3] UTF8String], [_spaces[4] UTF8String],[_spaces[5] UTF8String],[_spaces[6] UTF8String], [_spaces[7] UTF8String], [_spaces[8] UTF8String]);
 }
+//First Player "X" if statement
 
 - (void) chooseMove:(int)i {
     
     if ([_spaces[i] isEqualToString:_startingSpaces[i]]) {
         
-        [_spaces replaceObjectAtIndex: i withObject: @"x"];
+        [_spaces replaceObjectAtIndex: i withObject: @"X"];
     }
     
     [self printBoard];
+}
+//Second Player "O" if statement
+
+- (void) chooseMove2:(int)a  {
+    
+    if ([_spaces[a] isEqualToString:_startingSpaces[a]]) {
+        
+        [_spaces replaceObjectAtIndex: a withObject: @"O"];
+    }
+    
+    [self printBoard];
+    
 }
 
 @end
@@ -86,17 +101,23 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         int userInput;
-        
         Tictactoe *game1 = [[Tictactoe alloc] init];
+        
         [game1 printLabel];
+        
+        //While loop for repeat
+        
+        while (game1) {
         [game1 printBoard];
         printf("\nHello Player 'X'\nWhere would you like to play: ");
-        
         scanf("\n%d",&userInput);
-        
         [game1 chooseMove:userInput];
         
-        
+            [game1 printBoard];
+            printf("\nHello Player 'O'\nWhere would you like to play: ");
+            scanf("\n%d", &userInput);
+            [game1 chooseMove2:userInput];
+        }
         
     }
     return 0;
